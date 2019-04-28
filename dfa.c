60fa67bc129c656169e4c3cc49a75ac71b30b656
aris <aris.koning@monetdbsolutions.com>
2019-04-28 19:34:13 +0200

    Some refactoring.

--- dfa.c
+++ dfa.c
@@ -1,5 +1,5 @@
 /*
- * LIKE pattern parser based on a Deterministic Finite Automaton architecture.
+ * LIKE pattern parser based on Deterministic Finite Automaton architecture.
  */
 
 #include <monetdb_config.h>
@@ -8,6 +8,7 @@
 typedef char lchar_t;
 
 typedef enum cardinality {
+    UNDEFINED,
     GREATER_OR_EQUAL,
     EQUAL
 } cardinality_t;
@@ -61,7 +62,6 @@ transition_function_t from_normal_character_to_normal_character;
 transition_function_t from_first_escape_to_percentage;
 transition_function_t from_first_escape_to_underscore;
 transition_function_t from_first_escape_to_second_escape;
-transition_function_t from_first_escape_to_normal_character;
 transition_function_t from_first_escape_to_error;
 
 transition_function_t from_second_escape_to_percentage;
@@ -69,7 +69,7 @@ transition_function_t from_second_escape_to_underscore;
 transition_function_t from_second_escape_to_first_escape;
 transition_function_t from_second_escape_to_normal_character;
 
-const size_t INIT_BUFFER_SIZE = 128 * sizeof(lchar_t);
+const size_t INIT_BUFFER_SIZE = 128;
 
 static state_t* create_initial_state() {
     state_t* init_state;
@@ -78,8 +78,8 @@ static state_t* create_initial_state() {
     init_state = (state_t*) GDKmalloc(sizeof(state_t));
     first = (searchstring_t*) GDKmalloc(sizeof(searchstring_t));
     first->start = 0;
-    first->card = EQUAL;
-    first->string_buffer = (lchar_t*) GDKmalloc(INIT_BUFFER_SIZE);
+    first->card = UNDEFINED;
+    first->string_buffer = (lchar_t*) GDKmalloc(INIT_BUFFER_SIZE * sizeof(lchar_t));
     first->capacity = INIT_BUFFER_SIZE;
     first->ncharacters = 0;
     first->next = NULL;
@@ -88,6 +88,38 @@ static state_t* create_initial_state() {
     init_state->error_string = NULL;
 
     return init_state;
+}
+
+static void set_percentage_transition_functions(state_t* state) {
+    state->to_percentage        = from_percentage_to_percentage;
+    state->to_underscore        = from_percentage_to_underscore;
+    state->to_first_escape      = from_percentage_to_first_escape;
+    state->to_second_escape     = NULL;
+    state->to_normal_character  = from_percentage_to_normal_character;
+}
+
+static void set_underscore_transition_functions(state_t* state) {
+    state->to_percentage        = from_underscore_to_percentage;
+    state->to_underscore        = from_underscore_to_underscore;
+    state->to_first_escape      = from_underscore_to_first_escape;
+    state->to_second_escape     = NULL;
+    state->to_normal_character  = from_underscore_to_normal_character;
+}
+
+static void set_first_escape_transition_functions(state_t* state) {
+    state->to_percentage        = from_first_escape_to_percentage;
+    state->to_underscore        = from_first_escape_to_underscore;
+    state->to_first_escape      = NULL;
+    state->to_second_escape     = from_first_escape_to_second_escape;
+    state->to_normal_character  = from_first_escape_to_error;
+}
+
+static void set_normal_character_transition_functions(state_t* state) {
+    state->to_percentage        = from_normal_character_to_percentage;
+    state->to_underscore        = from_normal_character_to_underscore;
+    state->to_first_escape      = from_normal_character_to_first_escape;
+    state->to_second_escape     = NULL;
+    state->to_normal_character  = from_normal_character_to_normal_character;
 }
 
 state_t* start_with_percentage(lchar_t*  character) {
@@ -99,6 +131,8 @@ state_t* start_with_percentage(lchar_t*  character) {
     init_state->current->card = GREATER_OR_EQUAL;
     assert(init_state->current->start == 0);
 
+    set_percentage_transition_functions(init_state);
+
     return init_state;
 }
 
@@ -108,8 +142,11 @@ state_t* start_with_underscore(lchar_t*  character) {
     (void) character;
 
     init_state = create_initial_state();
-    assert(init_state->current->card == EQUAL);
+    init_state->current->card = EQUAL;
     assert(init_state->current->start == 0);
+    init_state->current->start++;
+
+    set_underscore_transition_functions(init_state);
 
     return init_state;
 }
@@ -120,7 +157,7 @@ state_t* start_with_normal_character(lchar_t*  character) {
     (void) character;
 
     init_state = create_initial_state();
-    assert(init_state->current->card == EQUAL);
+    init_state->current->card = EQUAL;
     assert(init_state->current->start == 0);
 
     return init_state;
